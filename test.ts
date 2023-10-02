2b32e4d1fddf9d247d58d2c10942428de86eb099
Alex Rocha <rocha.amc@gmail.com>
2023-10-02 12:12:07 -0700

    Improve test coverage

--- test.ts
+++ test.ts
@@ -6,3 +6,18 @@ test('Format phone number', () => {
   expect(helper.formatPhone(null)).toBe('() -');
   expect(helper.formatPhone('')).toBe('() -');
 });
+
+test('Capitalize strings', () => {
+  expect(helper.capitalize('neel')).toBe('Neel');
+  expect(helper.capitalize('Neel')).toBe('Neel');
+  expect(helper.capitalize('')).toBe('');
+  expect(helper.capitalize('n')).toBe('N');
+});
+
+test('Full name formatting', () => {
+  expect(helper.fullName('neel', 'mclarty')).toBe('Neel Mclarty');
+  expect(helper.fullName('NEEL', 'MCLARTY')).toBe('NEEL MCLARTY');
+  expect(helper.fullName('n', 'm')).toBe('N M');
+  expect(helper.fullName('', 'mclarty')).toBe(' Mclarty');
+  expect(helper.fullName('neel', '')).toBe('Neel ');
+});
