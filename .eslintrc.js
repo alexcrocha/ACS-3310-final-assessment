17e63edb05080c4e3a10241993ade9920dc45fd6
Alex Rocha <rocha.amc@gmail.com>
2023-10-02 10:31:12 -0700

    Add airbnb typescript eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ["airbnb-base",
+    "airbnb-typescript/base"],
+};
